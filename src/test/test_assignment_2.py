65bc36582514bf83c7e160f3bd2fcb9f261e00ae
Steven Grady <stevenagrady@gmail.com>
2025-02-15 17:35:36 -0500

    feat: add test for newton forward

--- src/test/test_assignment_2.py
+++ src/test/test_assignment_2.py
@@ -1,5 +1,5 @@
 import unittest
-from src.main.assignment_2 import neville_interpolation
+from src.main.assignment_2 import neville_interpolation, newton_forward_interpolation
 
 class TestNevilleInterpolation(unittest.TestCase):
     def test_neville_interpolation(self):
@@ -9,6 +9,19 @@ class TestNevilleInterpolation(unittest.TestCase):
         expected = 1.5549999999999995
         result = neville_interpolation(x_vals=x, y_vals=y, x_target=w)
         self.assertAlmostEqual(result, expected, places=5)
+    
+    def test_newton_forward_interpolation(self):
+        x = [7.2, 7.4, 7.5, 7.6]
+        y = [23.5492, 25.3913, 26.8224, 27.4589]
+        degree = 3
+        x_target = 7.3
+        expected_coeffs = [9.2105, 17.00166666666675, -141.82916666666722]
+        expected_f73 = 24.016574999999992
+        coefficients, _, approx_f73 = newton_forward_interpolation(x, y, degree, x_target)
+        
+        for i in range(1, len(expected_coeffs) + 1):
+            self.assertAlmostEqual(coefficients[i], expected_coeffs[i - 1], places=5)
+        self.assertAlmostEqual(approx_f73, expected_f73, places=5)
 
 if __name__ == "__main__":
     unittest.main()
